ee7eb5c2dd6cadf789b1a7feae6aafe895c27f43
hhh <1178919633@qq.com>
2017-12-28 12:07:23 +0800

    f

---
+++ database-util/src/main/java/cn/upc/database/constants/Constants.java
@@ -0,0 +1,6 @@
+package cn.upc.database.constants;
+
+public class Constants {
+
+	
+}
---
+++ database-util/src/main/java/cn/upc/database/utils/ResultData.java
@@ -0,0 +1,58 @@
+package cn.upc.database.utils;
+
+import java.io.Serializable;
+
+public class ResultData<T> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6079119599458665028L;
+
+	private T data;
+	
+	private String message;
+	
+	private boolean success;
+	
+	public Object getData() {
+		return data;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public ResultData(T data, String message, boolean success) {
+		this.data = data;
+		this.message = message;
+		this.success = success;
+	}
+
+	public ResultData(T data, String message) {
+		this.data = data;
+		this.message = message;
+		this.success = true;
+	}
+	
+	public ResultData(T data) {
+		this.data = data;
+		this.success = true;
+	}
+}
